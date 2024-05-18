f8c76be83a40be8624ae0317406348e100441a82
StephenJP14 <stephen.j.pratama@gmail.com>
2024-05-18 08:37:40 +0000

    update dashboard handler

--- handlers/connection.php
+++ handlers/connection.php
@@ -4,4 +4,5 @@ $servername = "localhost:3306";
 $sql_username = "stephen";
 $sql_password = "o8ivx1(EzV(I-9M4M7";
 $dbname = "stephen_db";
+
 ?>
--- handlers/dashboard_handler.php
+++ handlers/dashboard_handler.php
@@ -1,5 +1,4 @@
 <?php 
-
     session_start();
 
     $uid = $_SESSION['uid'];
