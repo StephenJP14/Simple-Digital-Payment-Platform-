c7c0a71ea5bc4d5c22c8a56078c547025ab61859
hideffrand <hideffrand@gmail.com>
2024-05-19 15:49:21 +0700

    fix login

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-  <link rel="stylesheet" href="./styles/login.css" />ini dah bener padahal
+  <link rel="stylesheet" href="./styles/login.css" />
   <title>Login</title>
 </head>
 
