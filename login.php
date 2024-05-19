017f8195dfecf3690252bb5d50a32ea44b774912
hideffrand <hideffrand@gmail.com>
2024-05-19 15:53:10 +0700

    fix login/reg

--- login.php
+++ login.php
@@ -13,29 +13,31 @@
 <body id="login">
   <?php
   session_start();
-  if (session_status() !== PHP_SESSION_NONE || isset($_SESSION['email'])) {
+  if (session_status() === PHP_SESSION_NONE || !isset($_SESSION['email'])) {
     // if (session_status() === PHP_SESSION_NONE){
     //     echo PHP_SESSION_NONE;
     // }
+    echo <<<END
+        <nav>
+          <a href="index.html">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </a>
+          <div class="logo"><span class="green-text">C</span>-Pay Login</div>
+        </nav>
+        <form action="./handlers/login_handler.php" method="POST">
+          <label for="email">Email:</label>
+          <input type="email" name="email" id="email" placeholder="you@gmail.com" />
+          <label for="password">Password:</label>
+          <input type="password" name="password" id="password" placeholder="..." />
+          <input type="submit" value="Login" id="submit" />
+          <br />
+          <a href="register.html">Dont have an account?</a>
+        </form>
+      END;
+  } else {
     header("Location: dashboard.php");
     exit();
   }
-  ?>
-  <nav>
-    <a href="index.html">
-      <ion-icon name="arrow-back-outline"></ion-icon>
-    </a>
-    <div class="logo"><span class="green-text">C</span>-Pay Login</div>
-  </nav>
-  <form action="./handlers/login_handler.php" method="POST">
-    <label for="email">Email:</label>
-    <input type="email" name="email" id="email" placeholder="you@gmail.com" />
-    <label for="password">Password:</label>
-    <input type="password" name="password" id="password" placeholder="..." />
-    <input type="submit" value="Login" id="submit" />
-    <br />
-    <a href="register.html">Dont have an account?</a>
-  </form>
   ?>
 </body>
 
