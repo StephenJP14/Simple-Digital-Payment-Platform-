8321258145571755f99743e1cb100f579790f560
hideffrand <hideffrand@gmail.com>
2024-05-19 19:37:11 +0700

    fix dashboard

--- dashboard.php
+++ dashboard.php
@@ -51,6 +51,7 @@
       </div>
     </div>
   </header>
+
   <section>
     <div class="container">
       <b>History:</b>
@@ -65,6 +66,7 @@
       </table>
     </div>
   </section>
+  
   <script>
     $.ajax({
       type: "GET",
