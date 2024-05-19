e2a0cb9855a59544b86197f3f1bde4cfab9200b7
hideffrand <hideffrand@gmail.com>
2024-05-19 15:46:49 +0700

    fix: login/reg css

--- login.php
+++ login.php
@@ -6,38 +6,36 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-  <link rel="stylesheet" href="./styles/login.css" />
+  <link rel="stylesheet" href="./styles/login.css" />ini dah bener padahal
   <title>Login</title>
 </head>
 
 <body id="login">
   <?php
   session_start();
-  if (session_status() === PHP_SESSION_NONE || !isset($_SESSION['email'])) {
+  if (session_status() !== PHP_SESSION_NONE || isset($_SESSION['email'])) {
     // if (session_status() === PHP_SESSION_NONE){
     //     echo PHP_SESSION_NONE;
     // }
-    echo <<<END
-        <nav>
-          <a href="index.html">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-          </a>
-          <div class="logo"><span class="green-text">C</span>-Pay Login</div>
-        </nav>
-        <form action="./handlers/login_handler.php" method="POST">
-          <label for="email">Email:</label>
-          <input type="email" name="email" id="email" placeholder="you@gmail.com" />
-          <label for="password">Password:</label>
-          <input type="password" name="password" id="password" placeholder="..." />
-          <input type="submit" value="Login" id="submit" />
-          <br />
-          <a href="register.html">Dont have an account?</a>
-        </form>
-      END;
-  } else {
     header("Location: dashboard.php");
     exit();
   }
+  ?>
+  <nav>
+    <a href="index.html">
+      <ion-icon name="arrow-back-outline"></ion-icon>
+    </a>
+    <div class="logo"><span class="green-text">C</span>-Pay Login</div>
+  </nav>
+  <form action="./handlers/login_handler.php" method="POST">
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" placeholder="you@gmail.com" />
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" placeholder="..." />
+    <input type="submit" value="Login" id="submit" />
+    <br />
+    <a href="register.html">Dont have an account?</a>
+  </form>
   ?>
 </body>
 
--- register.php
+++ register.php
@@ -12,47 +12,39 @@
 
 <body>
   <?php
-  if (session_status() === PHP_SESSION_NONE || !isset($_SESSION['email'])) {
-    echo <<<END
-        <nav>
-          <a href="index.html">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-          </a>
-          <div class="logo"><span class="green-text">C</span>-Pay Register</div>
-        </nav>
-        <form action="./handlers/register_handler.php" method="POST">
-          <label for="name">Name:</label>
-          <input type="text" name="name" id="name" placeholder="Your name" />
-    
-          <label for="address">Address:</label>
-          <input
-            type="text"
-            name="address"
-            id="address"
-            placeholder="Your address"
-          />
-    
-          <label for="phone">Phone:</label>
-          <input type="text" name="phone" id="phone" placeholder="you@gmail.com" />
-    
-          <label for="email">Email:</label>
-          <input type="email" name="email" id="email" placeholder="you@gmail.com" />
-    
-          <label for="password">Password:</label>
-          <input type="password" name="password" id="password" placeholder="..." />
-          <label for="password">Confirm password:</label>
-          <input type="password" name="password2" id="password" placeholder="..." />
-    
-          <input type="submit" value="Register" id="submit" />
-          <br />
-          <a href="login.html">Already have an account?</a>
-        </form>
-      END;
-  } else {
+  if (session_status() !== PHP_SESSION_NONE || isset($_SESSION['email'])) {
     header("Location: login.php");
     exit();
   }
   ?>
+  <nav>
+    <a href="index.html">
+      <ion-icon name="arrow-back-outline"></ion-icon>
+    </a>
+    <div class="logo"><span class="green-text">C</span>-Pay Register</div>
+  </nav>
+  <form action="./handlers/register_handler.php" method="POST">
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name" placeholder="Your name" />
+
+    <label for="address">Address:</label>
+    <input type="text" name="address" id="address" placeholder="Your address" />
+
+    <label for="phone">Phone:</label>
+    <input type="text" name="phone" id="phone" placeholder="you@gmail.com" />
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" placeholder="you@gmail.com" />
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" placeholder="..." />
+    <label for="password">Confirm password:</label>
+    <input type="password" name="password2" id="password" placeholder="..." />
+
+    <input type="submit" value="Register" id="submit" />
+    <br />
+    <a href="login.html">Already have an account?</a>
+  </form>
 </body>
 
 </html>
